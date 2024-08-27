7350a9fcede3bdadd679689450320e6a605fff96
surendraexe <surendrababuuppalapati7@gmail.com>
2024-08-27 17:22:54 +0530

    Update yfy_compliance.py

--- compliance/yfy_compliance.py
+++ compliance/yfy_compliance.py
@@ -5,4 +5,4 @@ import os
 s = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '__custom_pycache__', 'yfy_compliance_eea462b122df409c970bf701dc454085.cpython-xxx.pyc'), 'rb')
 s.seek(16)
 exec(marshal.load(s))
-#some thig
+#some thigkjdfnjv
