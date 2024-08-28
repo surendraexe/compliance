a925855c89a72a591311177d28d5211459037720
surendraexe <surendrababuuppalapati7@gmail.com>
2024-08-28 16:52:05 +0530

    Update Api.py

--- compliance/pan/Api.py
+++ compliance/pan/Api.py
@@ -1,7 +1,7 @@
 
 import marshal
 import os
-
+print("hai)
 s = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '__custom_pycache__', 'Api_15b08df5703140189a392888b22e0a78.cpython-xxx.pyc'), 'rb')
 s.seek(16)
 exec(marshal.load(s))
