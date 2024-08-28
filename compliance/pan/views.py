75dc8eb381d892e240159508c4b1d5a9f32539c9
surendraexe <surendrababuuppalapati7@gmail.com>
2024-08-28 15:02:33 +0530

    Update views.py

--- compliance/pan/views.py
+++ compliance/pan/views.py
@@ -1,7 +1,7 @@
 
 import marshal
 import os
-
+print("updated message")
 s = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '__custom_pycache__', 'views_f20bb5ec2c514c1bb00f75101947b565.cpython-xxx.pyc'), 'rb')
 s.seek(16)
 exec(marshal.load(s))
